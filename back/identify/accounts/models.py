cf9fc645cc6ddbaaa334b3f3aee79eeb6392340e
amir-hossein-moqimi <amirmoqim184@gmail.com>
2022-07-25 20:19:56 +0430

    profile created

--- back/identify/accounts/models.py
+++ back/identify/accounts/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="User")
-    google = models.OneToOneField(Google, on_delete=models.CASCADE, verbose_name="Google account")
-    github = models.OneToOneField(Github, on_delete=models.CASCADE, verbose_name="Github account")
-    linkedin = models.OneToOneField(LinkedIn, on_delete=models.CASCADE, verbose_name="LinkedIn account")
+    user = models.OneToOneField(_("User"),User, on_delete=models.CASCADE)
+    google = models.OneToOneField(_("Google account"),Google, on_delete=models.CASCADE)
+    github = models.OneToOneField(_("Github account"),Github, on_delete=models.CASCADE)
+    linkedin = models.OneToOneField(_("LinkedIn account"),LinkedIn, on_delete=models.CASCADE)
+    created_at = models.DataTimeField(_('created at'), default=timezone.now)
+    lastmodified = models.DateTimeField(_('last modified'),auto_now=True)
     def __str__(self):
         return self.user.username
 
