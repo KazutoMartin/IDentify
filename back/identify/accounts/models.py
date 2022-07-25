cbd4b80b0596ae86a2305130e0165b361e56b6b3
amir-hossein-moqimi <amirmoqim184@gmail.com>
2022-07-25 20:10:13 +0430

    profile created

--- back/identify/accounts/models.py
+++ back/identify/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -12,12 +12,20 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-class Google(models.Model):pass
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Google(models.Model):
+    pass
 
 class Github(models.Model):
     pass
-
 
 class LinkedIn(models.Model):
     pass
