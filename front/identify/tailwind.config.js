8825b1a5576853aaac6b294ecc08205277af63b3
KazutoMartin <75247334+KazutoMartin@users.noreply.github.com>
2022-07-27 08:20:37 +0430

    tailwind set up and tailwind.config.js configured

---
+++ front/identify/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    // colors: {
+    //   background: "#26272F",
+    //   purple: "#F473B9",
+    //   pink: "#FFBDE6",
+    //   white: "#FFFFFF",
+    //   lighpink: "#FFDDEE",
+    //   blue: "#0E3EDA",
+    //   green: "#13ce66",
+    //   yellow: "#ffc82c",
+    //   "gray-dark": "#273444",
+    //   gray: "#8492a6",
+    //   "gray-light": "#d3dce6",
+    // },
+    fontFamily: {
+      logo: ["Inter", "sans-serif"],
+      // serif: ["Merriweather", "serif"],
+    },
+    extend: { colors: { background: "#26272F", sea: "#0E3EDA" } },
+  },
+  plugins: [],
+};
