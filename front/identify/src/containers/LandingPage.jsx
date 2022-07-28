3abeed9f60a5e490dccc6c8c4d5f2058c5bc4eb0
KazutoMartin <75247334+KazutoMartin@users.noreply.github.com>
2022-07-28 10:48:44 +0430

    added really nice wavy background

--- front/identify/src/containers/LandingPage.jsx
+++ front/identify/src/containers/LandingPage.jsx
@@ -3,8 +3,11 @@ import Navbar from "../components/Navbar";
 
 function LandingPage() {
   return (
-    <div className="bg-background h-screen w-screen">
+    <div className="flex flex-col bg-background h-screen w-screen">
       <Navbar />
+      <div className="w-screen h-full">
+        <div className="h-3/4 w-full absolute bottom-0 bg-wave bg-no-repeat bg-center bg-cover"></div>
+      </div>
     </div>
   );
 }
--- front/identify/tailwind.config.js
+++ front/identify/tailwind.config.js
@@ -2,19 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    // colors: {
-    //   background: "#26272F",
-    //   purple: "#F473B9",
-    //   pink: "#FFBDE6",
-    //   white: "#FFFFFF",
-    //   lighpink: "#FFDDEE",
-    //   blue: "#0E3EDA",
-    //   green: "#13ce66",
-    //   yellow: "#ffc82c",
-    //   "gray-dark": "#273444",
-    //   gray: "#8492a6",
-    //   "gray-light": "#d3dce6",
-    // },
     fontFamily: {
       logo: ["Inter", "sans-serif"],
       font1: ["Open Sans", "sans-serif"],
@@ -23,6 +10,9 @@ module.exports = {
     },
     extend: {
       colors: { background: "#26272F", sea: "#0E3EDA", hoverbutton: "#26E0ED" },
+      backgroundImage: {
+        wave: "url('assets/landingbg.svg')",
+      },
     },
   },
   plugins: [],
