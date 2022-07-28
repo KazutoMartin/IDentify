76a309a1c13c9d9dc918cdedf08037dba763febc
amirHosseinRadmehrr <amirhradmehr132@gmail.com>
2022-07-28 21:27:49 +0430

    responsive navbar

--- front/identify/tailwind.config.js
+++ front/identify/tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
 
       // serif: ["Merriweather", "serif"],
     },
+
+    screens: {
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
+    
     extend: {
       colors: { background: "#26272F", sea: "#0E3EDA", hoverbutton: "#26E0ED" },
       backgroundImage: {
