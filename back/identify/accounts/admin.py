0c8234d8176582728e1da15f7e2d776e94429634
Game <Game@H1site09.rouzbeh.edu>
2022-08-02 11:48:26 +0430

    registeration and migrations of models completed

--- back/identify/accounts/admin.py
+++ back/identify/accounts/admin.py
@@ -4,6 +4,9 @@ from . import models
 # use admin.site.register(models.modelname) to register
 
 admin.site.register(models.Profile)
+
 admin.site.register(models.Google)
+
 admin.site.register(models.Github)
+
 admin.site.register(models.LinkedIn)
