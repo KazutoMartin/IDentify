427a7d3d04baf463aa4987b9cae5af754e3b3f4d
amirHosseinRadmehrr <amirhradmehr132@gmail.com>
2022-08-19 00:36:52 +0430

    signin page made responsive

--- front/identify/src/components/SignInForm.jsx
+++ front/identify/src/components/SignInForm.jsx
@@ -10,7 +10,7 @@ import LockIcon from '@mui/icons-material/Lock';
 
 function SignInForm() {
   return (
-  <div className="flex mx-auto bg-white w-1/3 h-1/2 rounded-xl justify-center flex-col z-50">
+  <div className="flex mx-auto bg-white w-1/3 h-1/2 rounded-xl justify-center flex-col sml:w-2/3 mt-20 z-50">
     <div className="w-full h-1/3 flex justify-center items-center"><h1 className="font-signfont text-4xl">Login</h1></div>
     <div className="w-full h-1/3 flex justify-center items-center">
     <Box sx={{ display: 'flex', alignItems: 'flex-end', width: '77%'}}>
@@ -25,7 +25,7 @@ function SignInForm() {
       </Box>
     </div>
     <div className="w-full h-1/6 flex justify-end">
-      <div className="h-full w-1/2"><a href="#" className="flex justify-center items-start w-full text-gray-500">forgot password?</a></div> 
+      <div className="h-full w-1/2"><a href="#" className="flex justify-center items-start w-full text-gray-500 mdl:text-xs">forgot password?</a></div> 
     </div>
     <div className="w-full h-1/6 flex justify-center items-center mb-4">
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-3/4">
