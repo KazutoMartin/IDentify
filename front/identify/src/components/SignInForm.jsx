60cf1225a267f2085886aee0360ecc5e12de14ca
amirHosseinRadmehrr <amirhradmehr132@gmail.com>
2022-08-19 00:53:45 +0430

    animation added to signin form

--- front/identify/src/components/SignInForm.jsx
+++ front/identify/src/components/SignInForm.jsx
@@ -7,11 +7,16 @@ import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LockIcon from '@mui/icons-material/Lock';
+import { motion } from "framer-motion";
 
 function SignInForm() {
   return (
-  <div className="flex mx-auto bg-white w-1/3 h-1/2 rounded-xl justify-center flex-col sml:w-2/3 mt-20 z-50">
-    <div className="w-full h-1/3 flex justify-center items-center"><h1 className="font-signfont text-4xl">Login</h1></div>
+  <motion.div 
+  initial={{ x: 0, opacity: 0, scale: 0.7, y: -500 }}
+  animate={{ x: 0, opacity: 1, scale: 1, y:0 }}
+  transition={{ ease: "backInOut", duration: 2 }}
+  className="flex mx-auto bg-white w-1/3 h-1/2 rounded-xl justify-center flex-col sml:w-2/3 mt-20 z-50">
+    <div className="w-full h-1/3 flex justify-center items-center"><h1 className="font-signfont text-4xl mdl:text-2xl">Login</h1></div>
     <div className="w-full h-1/3 flex justify-center items-center">
     <Box sx={{ display: 'flex', alignItems: 'flex-end', width: '77%'}}>
         <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5}} />
@@ -32,7 +37,7 @@ function SignInForm() {
   Login
 </button>
     </div>
-  </div>);
+  </motion.div>);
   
 }
 
