30fcc6dda67ba7e723295ff57ce7f0570535372f
amirHosseinRadmehrr <amirhradmehr132@gmail.com>
2022-08-19 02:39:25 +0430

    just some changes

--- front/identify/src/components/SignInForm.jsx
+++ front/identify/src/components/SignInForm.jsx
@@ -15,7 +15,7 @@ function SignInForm() {
   initial={{ x: 0, opacity: 0, scale: 0.7, y: -500 }}
   animate={{ x: 0, opacity: 1, scale: 1, y:0 }}
   transition={{ ease: "backInOut", duration: 2 }}
-  className="flex mx-auto bg-white w-1/3 h-1/2 rounded-xl justify-center flex-col sml:w-2/3 mt-20 z-50">
+  className="flex mx-auto bg-white w-1/3 h-1/2 rounded-xl justify-center flex-col sml:w-2/3 sml:mt-10 mt-20 z-50">
     <div className="w-full h-1/3 flex justify-center items-center"><h1 className="font-signfont text-3xl mdl:text-2xl">Login</h1></div>
     <div className="w-full h-1/3 flex justify-center items-center">
     <Box sx={{ display: 'flex', alignItems: 'flex-end', width: '77%'}}>
