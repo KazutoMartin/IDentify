62a338380234640cfa7497bb0260d169edbe0923
amir-hossein-moqimi <amirmoqim184@gmail.com>
2022-08-20 21:50:18 +0430

    fixed corsheaders whitelist

--- back/identify/identify/settings.py
+++ back/identify/identify/settings.py
@@ -181,8 +181,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
-       'http://localhost:3001/',
-       'http://127.0.0.1:3001/',
-       'http://localhost:3000/',
-       'http://127.0.0.1:3000/',
+       'http://localhost:3001',
+       'http://127.0.0.1:3001',
+       'http://localhost:3000',
+       'http://127.0.0.1:3000',
 )
